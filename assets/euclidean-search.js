f195c11926e25846020f744448c08d91e06bf3eb
Gabriel <gabrielperren@gmail.com>
2023-04-19 18:29:35 -0300

    test

--- assets/euclidean-search.js
+++ assets/euclidean-search.js
@@ -11,12 +11,12 @@ searchInput.addEventListener("input", e => {
 
   users.forEach(user => {
 
-    if (parseFloat(value.length) < 3) {
+    if (parseFloat(value.length) < 4) {
       user.element.classList.toggle("hide", true)
     } else {
       var distance = Math.sqrt(
-        Math.pow(parseFloat(xy[0]) - parseFloat(user.x), 2) +
-        Math.pow(parseFloat(xy[1]) - parseFloat(user.y), 2))
+        Math.pow(parseFloat(xy[0]) - parseFloat(user.ra), 2) +
+        Math.pow(parseFloat(xy[1]) - parseFloat(user.dec), 2))
       const isVisible =
         user.name.toLowerCase().includes(value) || distance <= 10
       user.element.classList.toggle("hide", !isVisible)
@@ -24,7 +24,7 @@ searchInput.addEventListener("input", e => {
   })
 })
 
-fetch("https://ucc.ar/_clusters/clusters.json")
+fetch("https://raw.githubusercontent.com/ucc23/ucc/main/clusters.json")
   .then(res => res.json())
   .then(data => {
     users = data.map(user => {
@@ -32,14 +32,14 @@ fetch("https://ucc.ar/_clusters/clusters.json")
       const header = card.querySelector("[data-header]")
       const body = card.querySelector("[data-body]")
       card.querySelector("a").setAttribute("href", "https://ucc.ar/_clusters/" + user.url)
-      header.textContent = user.name
-      body.textContent = user.company
+      header.textContent = user.ID
+      body.textContent = user.UCC_ID
       card.classList.add('hide');
       userCardContainer.append(card)
       return {
-        name: user.name,
-        x: user.x,
-        y: user.y,
+        name: user.ID,
+        ra: user.RA_ICRS,
+        dec: user.DE_ICRS,
         element: card
       }
     })
