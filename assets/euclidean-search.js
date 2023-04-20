a8cd74a30285ba696f6916acc0411f5914c32a1a
Gabriel <gabrielperren@gmail.com>
2023-04-20 17:29:34 -0300

    test

--- assets/euclidean-search.js
+++ assets/euclidean-search.js
@@ -3,17 +3,43 @@ const userCardContainer = document.querySelector("[data-user-cards-container]")
 const searchInput = document.querySelector("[data-search]")
 
 let users = []
+let results = []
+
+// searchInput.addEventListener("input", e => {
+
+//   const value = e.target.value.toLowerCase()
+//   const xy = value.split(' ')
+
+//   users.forEach(user => {
+
+//     if (parseFloat(value.length) < 4) {
+//       user.element.classList.toggle("hide", true)
+//     } else {
+//       if (xy[0] == "g") {
+//         var distance = Math.sqrt(
+//           Math.pow(parseFloat(xy[1]) - parseFloat(user.lon), 2) +
+//           Math.pow(parseFloat(xy[2]) - parseFloat(user.lat), 2))
+//       } else{
+//         var distance = Math.sqrt(
+//           Math.pow(parseFloat(xy[0]) - parseFloat(user.ra), 2) +
+//           Math.pow(parseFloat(xy[1]) - parseFloat(user.dec), 2))
+//       }
+//       const isVisible = user.name.toLowerCase().includes(value) || distance <= 0.17
+//       user.element.classList.toggle("hide", !isVisible)
+//     }
+//   })
+// })
 
 searchInput.addEventListener("input", e => {
+  results = []; //quickly empty array
+  userCardContainer.innerHTML = "";
 
   const value = e.target.value.toLowerCase()
   const xy = value.split(' ')
 
-  users.forEach(user => {
+  if (parseFloat(value.length) >= 4) {
+    users.forEach(user => {
 
-    if (parseFloat(value.length) < 4) {
-      user.element.classList.toggle("hide", true)
-    } else {
       if (xy[0] == "g") {
         var distance = Math.sqrt(
           Math.pow(parseFloat(xy[1]) - parseFloat(user.lon), 2) +
@@ -24,10 +50,24 @@ searchInput.addEventListener("input", e => {
           Math.pow(parseFloat(xy[1]) - parseFloat(user.dec), 2))
       }
       const isVisible = user.name.toLowerCase().includes(value) || distance <= 0.17
-      user.element.classList.toggle("hide", !isVisible)
-    }
-  })
+      // user.element.classList.toggle("hide", !isVisible)
+
+      if (isVisible) {
+        user.distance = distance
+        results.push(
+          user
+        );
+      }
+    })
+  }
+
+  results.sort((a, b) => a.distance - b.distance)
+  results = results.slice(0, 10)
+  results.forEach((u) => {
+    userCardContainer.append(u.element);
+  });
 })
+
 
 fetch("/_clusters/clusters.json")
   .then(res => res.json())
@@ -39,8 +79,8 @@ fetch("/_clusters/clusters.json")
       card.querySelector("a").setAttribute("href", "https://ucc.ar/_clusters/" + user.fname)
       header.textContent = user.ID
       body.textContent = user.UCC_ID
-      card.classList.add('hide');
-      userCardContainer.append(card)
+      // card.classList.add('hide');
+      // userCardContainer.append(card)
       return {
         name: user.ID,
         ra: user.RA_ICRS,
