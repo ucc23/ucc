dcc0abd0a2c1d2af54b18332cc9a341e320549d4
Gabriel <gabrielperren@gmail.com>
2023-08-08 18:38:53 -0300

    script to produce background image

--- images/background.py
+++ images/background.py
@@ -2,47 +2,34 @@
 import pandas as pd
 from astropy import units as u
 from astropy.coordinates import SkyCoord
-# import astropy.coordinates as coord
 import matplotlib.pyplot as plt
-import matplotlib.gridspec as gridspec
 import numpy as np
 
+"""
+After generating this image, move the plot from the center of the frame to
+the bottom left and trim the blank space to the right. This generates an
+image with the proper dimensions for the ucc.ar site.
+"""
 
-def main():
-    """
-    Gridspec idea: http://www.sc.eso.org/~bdias/pycoffee/codes/20160407/
-                   gridspec_demo.html
-    """
+df = pd.read_csv("../../add_New_DB/UCC_cat_230702.csv")
 
-    # gc_frame = coord.Galactocentric()
+sizes = 1 * (1 / np.clip(df['plx'].values, .1, 20))
 
-    df = pd.read_csv("../../add_New_DB/UCC_cat_230702.csv")
+# Galactic coordinates.
+eq = SkyCoord(ra=df['RA_ICRS']*u.degree, dec=df['DE_ICRS']*u.degree, frame='icrs')
+lb = eq.transform_to('galactic')
+lon = lb.l.wrap_at(180 * u.deg).radian * u.radian
+lat = lb.b.radian * u.radian
 
-    sizes = 1 * (1 / np.clip(df['plx'].values, .1, 20))
+# invert lon
+i_lon = -lon
 
-    # Galactic coordinates.
-    eq = SkyCoord(ra=df['RA_ICRS']*u.degree, dec=df['DE_ICRS']*u.degree, frame='icrs')
-    lb = eq.transform_to('galactic')
-    lon = lb.l.wrap_at(180 * u.deg).radian * u.radian
-    lat = lb.b.radian * u.radian
+fig = plt.figure(figsize=(4, 4))
+ax = plt.subplot(111, projection="aitoff")
 
-    # invert lon
-    i_lon = -lon
+ax.grid(True, lw=.1)
+ax.scatter(i_lon, lat, s=sizes, lw=.2, facecolor='none', ec='grey', zorder=5)
+ax.set_xticklabels([])
+ax.set_yticklabels([])
 
-    fig = plt.figure(figsize=(4, 4))
-    ax = plt.subplot(111, projection="aitoff")
-    # ax = axes[-1]
-
-    ax.grid(True, lw=.1)
-    ax.scatter(i_lon, lat, s=sizes, lw=.2, facecolor='none', ec='grey', zorder=5)
-    ax.set_xticklabels([])
-    ax.set_yticklabels([])
-
-    # # fig.tight_layout()
-    fig.savefig('background.webp', dpi=200)#, bbox_inches='tight')
-
-
-if __name__ == '__main__':
-    # import scienceplots
-    # plt.style.use('science')
-    main()
+fig.savefig('background.webp', dpi=150)
